4b34d1004efc3a8a9758bafe4693926080810ad9
GrooT921 <rarapin96@gmail.com>
2022-09-18 18:25:00 +0200

    hw-1-react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import Profile from './UserCard';
+import Statistics from './UserStatistics';
+import FriendList from './FriendListItem';
+import user from '../user.json';
+import data from '../data.json';
+import friends from "../friends.json";
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+
+      <Statistics
+        stats={data}
+      />
+
+      <FriendList
+        friends={friends}
+      />
     </div>
+
   );
 };
---
+++ src/components/FriendListItem.jsx
@@ -0,0 +1,14 @@
+export default function FriendListItem({ friends }) {
+    return (
+        <ul class="friend-list">
+            {friends.map(friend =>
+            <li class="item" key={friend.id}>
+                    <span class="status">{friend.isOnline}</span>
+                    <img class="avatar-friends" src={friend.avatar} alt={friend.name} width="48" />
+                <p class="name-friends">{friend.name}</p>
+            </li>
+        )}
+        
+    </ul>
+    )
+}
